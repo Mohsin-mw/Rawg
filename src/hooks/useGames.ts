9526ed4f3cd73d66077d5eb6a3bd18fe09c23b7f
MMW <null.mmw@gmail.com>
2023-06-24 11:59:28 -0700

    Created custom Hook to fetch Games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const controller = new AbortController();
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
