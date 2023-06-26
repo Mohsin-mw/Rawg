0ec25dc486c08841af651a7d104759804034263a
MMW <null.mmw@gmail.com>
2023-06-26 15:17:57 -0700

    Image Added to Side bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,18 @@ import GamesGrid from "./components/GamesGrid";
 import GenreList from "./components/GenreList";
 function App() {
   return (
-    <Grid templateAreas={{ base: `"nav main"`, lg: `"nav nav" "aside main"` }}>
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
       <GridItem area="nav">
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,26 @@
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-urls";
 
 const GenreList = () => {
   const { data } = useGenre();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              objectFit="cover"
+              boxSize="32px"
+              borderRadius={8}
+            />
+            <Text fontSize="small">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
