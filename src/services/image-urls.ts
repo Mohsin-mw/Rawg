9989bda0a736f89ff101f148cbbed84930fdab42
MMW <null.mmw@gmail.com>
2023-06-30 11:10:08 -0700

    Handle games with no image

--- src/services/image-urls.ts
+++ src/services/image-urls.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/no-image-placeholder.webp";
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
 
