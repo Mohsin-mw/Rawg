5b9795cb308a465ea709d702df0be351d9a04ed2
MMW <null.mmw@gmail.com>
2023-06-30 12:43:55 -0700

    Customized the theme

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,9 +1,8 @@
 import { Text, SimpleGrid } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenre";
 import { GameQuery } from "../App";
 
 interface Props {
